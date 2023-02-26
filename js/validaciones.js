5e5836884d2fca3414801aaa99364fd8645dd9f9
Bernardo5 <bernardpena@gmail.com>
2023-02-26 13:05:45 -0300

    mensaje de error personalizados

--- js/validaciones.js
+++ js/validaciones.js
@@ -11,13 +11,75 @@ inputNacimiento.addEventListener("blur", (evento) =>{
     const tipoDeInput = input.dataset.tipo;
     if(validadores[tipoDeInput]){
         validadores[tipoDeInput](input);
+    }
 
+//! IF solo para verificar si es valido o el validity es valido.
+    if(input.validity.valid){
+        input.parentElement.classList.remove("input-container--invalid");
+        //? mensaje en caso de no encontrar un error
+        input.parentElement.querySelector(".input-message-error").innerHTML = "";
+    }else{
+        input.parentElement.classList.add("input-container--invalid");
+        //? mensaje en caso de encontrar un error
+        input.parentElement.querySelector(".input-message-error").innerHTML = mostrarMensajeDeError(tipoDeInput, 
+        input);
+        
     }
 }
+
+const tipoDeErrores = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "customError",
+
+]
+
+
+
+
+
+//? Personalizando los mensajes para el usuario
+const mensajesDeError ={
+    nombre: {
+        valueMissing: "Este Campo no puede estar Vacío"
+    },
+    email: {
+        valueMissing: "Este Campo no puede estar Vacío",
+        typeMismatch: "El Correo no es valido"
+    },
+    password: {
+        valueMissing: "Este Campo no puede estar Vacío",
+        patternMismatch: "Al menos 6 caracteres, Máximo 12, debe contener una letra minúscula, una letra mayúscula, un número y no puede contener caracteres especiales."
+    },
+    nacimiento: {
+        valueMissing: "Este campo no puede estar Vacio",
+        customError: "Debes tener al menos 18 años de Edad!"
+    },
+
+};
+
+
 
 const validadores = {
     nacimiento: input => validarNacimiento(input),
 };
+
+function mostrarMensajeDeError(tipoDeInput, input){
+    let mensaje = "";
+    tipoDeErrores.forEach( error =>{
+        if(input.validity[error]){
+            console.log(tipoDeInput, error);
+            console.log(input.validity[error]);
+            console.log(mensajesDeError[tipoDeInput][error]);
+            mensaje = mensajesDeError[tipoDeInput][error];
+            
+        }
+    } )
+
+    return mensaje;
+}
+
 
 function validarNacimiento(input){
     const fechaCliente = new Date(input.value + "T00:00:00");
